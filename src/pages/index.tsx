5dcd97a563b36bfc2cc05b587d324427e259498a
Dallin parker <dallin.r.parker@icloud.com>
2019-03-29 12:56:02 -0400

    removed graphql source plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,10 @@ import { LandingPage } from '../Layouts/IndexLayout';
 import Image from '../components/Image';
 import GivhrLogo from '../assets/images/givhr_text_logo.png';
 import Button from '../components/Button';
-import { StaticQuery, graphql, navigate } from 'gatsby';
 
 const IndexPage: React.FunctionComponent = () => (
   <LandingPage>
-    <StaticQuery
-      query={graphql`
-        query GivhrTest {
-          givhr {
-            surveys {
-              id
-              title
-            }
-          }
-        }
-      `}
-      render={({ givhr }) => {
-        console.log(givhr);
-        return givhr.surveys.map((survey) => (
-          <>
-            <div>ID: {survey.id}</div>
-            <div>TITLE: {survey.title}</div>
-          </>
-        ));
-      }}
-    />
+    <h1>Hello</h1>
   </LandingPage>
 );
 
