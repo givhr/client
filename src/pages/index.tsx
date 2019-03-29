28902fad3ef5aec18d9e66640664f07689452d1f
Dallin parker <dallin.r.parker@icloud.com>
2019-03-29 15:13:36 -0400

    added axios and basic requests are fetching properly

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,30 @@ import { LandingPage } from '../Layouts/IndexLayout';
 import Image from '../components/Image';
 import GivhrLogo from '../assets/images/givhr_text_logo.png';
 import Button from '../components/Button';
+import { getSurveys, updateSurvey } from '../services/api';
 
-const IndexPage: React.FunctionComponent = () => (
-  <LandingPage>
-    <h1>Hello</h1>
-  </LandingPage>
-);
+// const IndexPage: React.FunctionComponent = () => (
+//   <LandingPage>
+//     <h1>Hello</h1>
+//   </LandingPage>
+// );
+
+class IndexPage extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      surveys: ['Nothing']
+    };
+  }
+
+  componentDidMount() {
+    // getSurveys();
+    updateSurvey(2, { title: 'dallin' });
+  }
+  render() {
+    return <div>Hello</div>;
+  }
+}
 
 export default IndexPage;
---
+++ src/services/api/index.ts
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://givhr-api.herokuapp.com/api/v1';
+
+export const getSurveys = () => {
+  axios
+    .get(`${BASE_URL}/surveys`)
+    .then((data) => {
+      console.log('GET SURVEY DATA: ', data);
+    })
+    .catch((err) => console.log('GET SURVEY ERR >', err));
+};
+
+export const updateSurvey = (id: number, updates: any) => {
+  axios
+    .put(`${BASE_URL}/surveys/${id}`, updates)
+    .then((data) => {
+      console.log('UPDATE SURVEY DATA: ', data);
+      // SINGLE SURVEY RESPONSE
+    })
+    .catch((err) => console.log('UPDATE SURVEY ERR >', err));
+};
+
+export const createSurvey = () => {
+  axios
+    .put(`${BASE_URL}/surveys/${id}`, updates)
+    .then((data) => {
+      console.log('UPDATE SURVEY DATA: ', data);
+      // SINGLE SURVEY RESPONSE
+    })
+    .catch((err) => console.log('UPDATE SURVEY ERR >', err));
+};
+
+export const getUser = () => {
+  axios
+    .get(`${BASE_URL}`)
+    .then((data) => {
+      console.log('GET USER DATA: ', data);
+    })
+    .catch((err) => console.log('GET USER ERR >', err));
+};
+
+export const deleteSurvey = () => {
+  axios
+    .get(`${BASE_URL}`)
+    .then((data) => {
+      console.log('DELETE SURVEY DATA: ', data);
+    })
+    .catch((err) => console.log('DELETE SURVEY ERR >', err));
+};
